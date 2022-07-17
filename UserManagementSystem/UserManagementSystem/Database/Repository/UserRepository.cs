dcfaf7d5fc4f03463ad231a553982210b41a1075
KamilMammadov <kamiltm@code.edu.az>
2022-07-17 15:01:36 +0400

    UserRepository created

---
+++ UserManagementSystem/UserManagementSystem/Database/Repository/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagementSystem.Database.Models;
+
+namespace UserManagementSystem.Database.Repository
+{
+    class UserRepository
+    {
+
+        public static List<User> Users { get; set; } = new List<User>();
+
+
+        public static User Add(string name,string surname,string email,string password)
+        {
+            User firstUser = new User(name, surname, email, password);
+            Users.Add(firstUser);
+            Console.WriteLine("You successfully registered, now you can login with your new account!");
+            return firstUser;
+        }
+
+
+
+    }
+}
